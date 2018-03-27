14481e4f758d74b4cc0fae30edd44dcfa565bfef
shinina-muthiora <35975256+shinina-muthiora@users.noreply.github.com>
2018-03-27 15:48:21 -0700

    c51_ally

--- __C51.bat
+++ __C51.bat
@@ -1,11 +1,11 @@
 @echo off
 ::This file was created automatically by CrossIDE to compile with C51.
 C:
-cd "\Users\allisony\Documents\ElecRobot\"
-"C:\CrossIDE\Call51\Bin\c51.exe" --use-stdout ht (c) 2010-2018 Jesus Calvino-Fraga "C:\Users\allisony\Documents\ElecRobot\robotControl.c"
+cd "\Users\SHININA\Documents\GitHub\ElecRobot\"
+"C:\CrossIDE\Call51\Bin\c51.exe" --use-stdout  "C:\Users\SHININA\Documents\GitHub\ElecRobot\reciever_so_far.c"
 if not exist hex2mif.exe goto done
-if exist robotControl.ihx hex2mif robotControl.ihx
-if exist robotControl.hex hex2mif robotControl.hex
+if exist reciever_so_far.ihx hex2mif reciever_so_far.ihx
+if exist reciever_so_far.hex hex2mif reciever_so_far.hex
 :done
 echo done
-echo Crosside_Action Set_Hex_File C:\Users\allisony\Documents\ElecRobot\robotControl.hex
+echo Crosside_Action Set_Hex_File C:\Users\SHININA\Documents\GitHub\ElecRobot\reciever_so_far.hex
